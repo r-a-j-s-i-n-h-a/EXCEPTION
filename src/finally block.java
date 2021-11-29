b1f82f609d62f245aa675c2bceb23d4386cad904
r-a-j-s-i-n-h-a <94060322+r-a-j-s-i-n-h-a@users.noreply.github.com>
2021-11-29 23:45:59 +0530

    Create finally block.java

---
+++ src/finally block.java
@@ -0,0 +1,19 @@
+class TestFinallyBlock {    
+  public static void main(String args[]){    
+  try{    
+ 
+   int data=25/5;    
+   System.out.println(data);    
+  }    
+  
+  catch(NullPointerException e){  
+System.out.println(e);  
+}    
+
+ finally {  
+System.out.println("finally block is always executed");  
+}    
+    
+System.out.println("rest of phe code...");    
+  }    
+}    
